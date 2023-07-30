1475365a161f803e14ab2d4dc704277ccbb9ca6f
Mathias Gajhede <mag@venturecapitals.dk>
2023-07-31 01:20:36 +0200

    Update import.php

--- import.php
+++ import.php
@@ -1,3 +1,3 @@
 <?php
-rename("ippgateway", $cart_file_handling);
+rename($temp_folder_name."ippgateway", $cart_file_handling);
 rename("$cart_file_handling/ippgateway.php", $cart_file_handling."/$cart_file_handling.php");
